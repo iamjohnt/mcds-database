873979761cc46ad619323c83708448789b9e0cd7
johnt <tranjohn123@gmail.com>
2021-10-18 09:08:56 -0400

    added cross origin to endpoints

--- src/main/java/com/resource/Endpoint.java
+++ src/main/java/com/resource/Endpoint.java
@@ -8,6 +8,7 @@ import com.pojo.CaseDataRecords;
 import com.pojo.DeathDataRecords;
 import com.pojo.HospDataRecords;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,29 +20,34 @@ public class Endpoint {
     @Autowired
     private DbService dbService;
 
+    @CrossOrigin
     @GetMapping("/database/test")
     public String helloWorldDatabase() {
         return "Hello World Database";
     }
 
+    @CrossOrigin
     @GetMapping("/database/apriltest")
     public String getAprilFirstData() {
         CaseData thing = dbService.getAprilFirstCaseData();
         return thing.getCasesNew().toString();
     }
 
+    @CrossOrigin
     @GetMapping("/database/cases")
     public List<CaseData> getCaseDataRecords() {
         List<CaseData> records = dbService.getCaseDataList();
         return records;
     }
 
+    @CrossOrigin
     @GetMapping("/database/deaths")
     public List<DeathData> getDeathDataRecords() {
         List<DeathData> records = dbService.getDeathDataList();
         return records;
     }
 
+    @CrossOrigin
     @GetMapping("database/hospitalizations")
     public List<HospData>  getHospDataRecords() {
         List<HospData> records = dbService.getHospDataList();
